aabb24e1a42326a6b1a4ad3c938dfcc901dca42e
AlexDecker <alexanderdecker@dcc.ufmg.br>
2019-03-21 16:27:35 -0300

    new configs

--- params/params1.m
+++ params/params1.m
@@ -1,5 +1,5 @@
 %Parammeter function for dummie application
 function params = params1()
-	params.voltage = [1,5];%voltage range (V)
+	params.voltage = [5,12];%voltage range (V)
 	params.timeSkip = 1;%time interval between voltage updates (s)
 end
