3384ac0e5463752fbfbc218085523826b0d3880c
AlexDecker <alexdeckersousa@hotmail.com>
2020-03-25 00:10:52 -0300

    fine adjustment working now

--- optimizers/utils/fine_adjustment.m
+++ optimizers/utils/fine_adjustment.m
@@ -9,166 +9,72 @@
 %and the solution dv=0 is near the optimum by hypothesis
 function dv_values = fine_adjustment(Z, v, it, ir, It, Ir, P, tolerance, max_iterations)
     
+	%the return values of voltage adjustment: [] means no found result
     dv_values = [];
     
+	%number of TX and RX elements
     nt = length(it);
     nr = length(ir);
+	
+	iZ = eye(nt+nr)/Z;
+	
+	%dit = Z1*dv
+	Z1 = iZ(1:nt, 1:nt);
+	%dir = Z2*dv
+	Z2 = iZ(nt+1:end, 1:nt);
+	
+	%decision variables
+	x = [zeros(nt,1);P - v.'*it; It.^2 - abs(it).^2];
+	
+	%error vector
+	F = zeros(nt+nr+1,1);
+	
+	%Jacobian
+	J = zeros(nt+nr+1,nt+1+nt);
+		
+	for i = 1:max_iterations
+	
+		%calculating next error vector
+		%tx current
+		for t=1:nt
+			z = [Z1(t,:), 0, zeros(1,nt)];
+			F(t) = (it(t)'*z + (it(t).')*((z').'))*x + x.'*(z'*z + diag([zeros(nt,1);0;ones(nt,1)]))*x + abs(it(t))^2 - It(t)^2;
+		end
+		
+		%rx current
+		for r=1:nr
+			z = [Z2(r,:), 0, zeros(1,nt)];
+			F(nt+r) = (ir(r)'*z + (ir(r).')*((z').'))*x + x.'*(z'*z)*x + abs(ir(r))^2 - Ir(r)^2;
+		end
+		
+		Z3 = [Z1, zeros(nt,nt+1);
+			  zeros(nt+1,nt), zeros(nt+1)];
+		
+		%active power
+		F(end) = v.'*it + [v.'*Z1+it.',0,zeros(1,nt)]*x + x.'*(Z3+diag([zeros(nt,1);1;zeros(nt,1)]))*x - P;
+		
+		if max(abs(F))<tolerance
+			dv_values = x(1:nt);
+			break;
+		end
+		
+		%calculating next Jacobian
+		%tx current
+		for t=1:nt
+			z = [Z1(t,:), 0, zeros(1,nt)];
+			J(t,:) = it(t)'*z + (it(t).')*((z').') + x.'*(z'*z + (z'*z).' + 2*diag([zeros(nt,1);0;ones(nt,1)]));
+		end
+		
+		%rx current
+		for r=1:nr
+			z = [Z2(t,:), 0, zeros(1,nt)];
+			J(nt+r,:) = ir(r)'*z + (ir(r).')*((z').') + x.'*(z'*z + (z'*z).');
+		end
+		
+		J(end,:) = [v.'*Z1+it.',0,zeros(1,nt)] + x.'*(Z3+Z3.'+2*diag([zeros(nt,1);1;zeros(nt,1)]));
+		
+		%next solution
+		x = x - pinv(J)*F;
+	end
 
-    %mapping the variables into the x vector
-    %x = [dv; dit_real; dit_imag; dir_real; dir_imag; power_slack; it_slack]
-    
-    dv.len = nt;
-    dv.begin = 1;
-    dv.end = dv.begin + dv.len - 1;
-    
-    dit_real.len = nt;
-    dit_real.begin = dv.end + 1;
-    dit_real.end = dit_real.begin + dit_real.len - 1;
-    
-    dit_imag.len = nt;
-    dit_imag.begin = dit_real.end + 1;
-    dit_imag.end = dit_imag.begin + dit_imag.len - 1;
-    
-    dir_real.len = nr;
-    dir_real.begin = dit_imag + 1;
-    dir_real.end = dir_real.begin + dir_real.len - 1;
-
-    dir_imag.len = nr;
-    dir_imag.begin = dir_real.end + 1;
-    dir_imag.end = dir_imag.begin + dir_imag.len - 1;
-
-    power_slack.len = 1;
-    power_slack.begin = dir_imag.end + 1;
-    power_slack.end = power_slack.begin + power_slack.len - 1;
-
-    it_slack.len = nt;
-    it_slack.begin = power_slack.end + 1;
-    it_slack.end = it_slack.begin + it_slack.len - 1;
-
-    x = zeros(dv.len + dit_real.len + dit_imag.len + dir_real.len + dir_imag.len +...
-        power_slack.len + it_slack.len, 1);
-
-    %mapping the equations into F vector
-    target_current.len = nr;
-    target_current.begin = 1;
-    target_current.end = target_current.begin + target_current.len - 1;
-
-    active_power.len = 1;
-    active_pover.begin = target_current.end + 1;
-    active_power.end = active_power.begin + active_power.len - 1;
-
-    tx_current.len = nt;
-    tx_current.begin = active_power.end + 1;
-    tx_current.end = tx_current.begin + tx_current.len - 1.
-
-    real_voltage.len = nt+nr;
-    real_voltage.begin = tx_current.end + 1;
-    real_voltage.end = real_voltage.begin + real_voltage.len - 1;
-
-    imag_voltage.len = nt+nr;
-    imag_voltage.begin = real_voltage.end + 1;
-    imag_voltage.end = imag_voltage.begin + imag_voltage.len - 1;
-
-    F = zeros(target_current.len + active_power.len + transmitting_current +...
-        real_voltage.len + imag_voltage.len, 1);
-
-    %Jacobian
-    J = zeros(length(x),length(F));
-    
-    for k=1:max_iterations
-        
-        dv_ = x(dc.begin:dv.end);
-        
-        dit_r = x(dit_real.begin:dit_real.end);
-        dit_i = x(dit_imag.begin:dit_imag.end);
-        dit = dit_r + 1i*dit_i;
-
-        dir_r = x(dir_real.begin:dir_real.end);
-        dir_i = x(dir_imag.begin:dir_imag.end);
-        dir = dir_r + 1i*dir_i;
-
-        s_p = x(power_slack.begin:power_slack.end);
-        s_t = x(it_slack.begin:it_slack.end);
-
-        %calculating the error vector
-        F(target_current.begin:target_current.end) = abs(ir + dir).^2-Ir.^2;
-        F(active_power.begin:active_power.end) = (v + dv_).'*(real(it) + dit) + s_p.^2 - P;
-        F(tx_current.begin:tx_current.end) = abs(ir + dir).^2 + s_t.^2 - It.^2;
-        F(real_voltage.begin:real_voltage.end) = [v + dv_; zeros(nr,1)] - ...
-            real(Z)*[real(it)+dit_r ; real(ir)+dir_r] + ...
-            imag(Z)*[imag(it)+dit_i ; imag(ir)+dir_i]; 
-        F(imag_voltage.begin:imag_voltage.end) = imag(Z) * [real(it)+dit_r ; real(ir)+dir_r] + ...
-            real(Z) * [imag(it)+dit_i ; imag(ir)+dir_i];
-
-        %stop condition
-        if max(abs(F)) < tolerance
-            dv_values = dv_;
-            break;
-        end
-
-        %calculating the Jacobian
-        J(target_current.begin:target_current.end, dv.begin:dv.end) = ...
-            zeros(target_current.len,dv.len);
-        J(target_current.begin:target_current.end, dit_real.begin:dit_real.end) = ...
-            zeros(target_current.len,dit_real.len);
-        J(target_current.begin:target_current.end, dit_imag.begin:dit_imag.end) = ...
-            zeros(target_current.len,dit_imag.len);
-        J(target_current.begin:target_current.end, dir_real.begin:dir_real.end) = ...
-            diag(2*real(ir)) + diag(2*dir_r);
-        J(target_current.begin:target_current.end, dir_imag.begin:dir_imag.end) = ...
-            diag(2*imag(ir)) + diag(2*dir_i);
-        J(target_current.begin:target_current.end, power_slack.begin:power_slack.end) = ...
-            zeros(target_current.len,power_slack.len);
-        J(target_current.begin:target_current.end, it_slack.begin:it_slack.end) = ...
-            zeros(target_current.len,it_slack.len);
-        
-        J(active_power.begin:active_power.end, dv.begin:dv.end) = ...
-            diag(dit_r) + diag(real(it));
-        J(active_power.begin:active_power.end, dit_real.begin:dit_real.end) = ...
-            diag(v) + diag(dv);
-        J(active_power.begin:active_power.end, dit_imag.begin:dit_imag.end) = ...
-            zeros(active_power.len,dit_imag.len);
-        J(active_power.begin:active_power.end, dir_real.begin:dir_real.end) = ...
-            zeros(active_power.len,dir_real.len);
-        J(active_power.begin:active_power.end, dir_imag.begin:dir_imag.end) = ...
-            zeros(active_power.len,dir_imag.len);
-        J(active_power.begin:active_power.end, power_slack.begin:power_slack.end) = ...
-            diag(2*sp);
-        J(active_power.begin:active_power.end, it_slack.begin:it_slack.end) = ...
-            zeros(active_power.len,it_slack.len);
-        
-        J(tx_current.begin:tx_current.end, dv.begin:dv.end) = ...
-            zeros(tx_current,dv.len);
-        J(tx_current.begin:tx_current.end, dit_real.begin:dit_real.end) = ...
-            diag(2*real(it)) + diag(2*dit_r);
-        J(tx_current.begin:tx_current.end, dit_imag.begin:dit_imag.end) = ...
-            diag(2*imag(it)) + diag(2*dit_i);
-        J(tx_current.begin:tx_current.end, dir_real.begin:dir_real.end) = ...
-            zeros(tx_current.len,dir_real.len);
-        J(tx_current.begin:tx_current.end, dir_imag.begin:dir_imag.end) = ...
-            zeros(tx_current.len,dit_imag.len);
-        J(tx_current.begin:tx_current.end, power_slack.begin:power_slack.end) = ...
-            zeros(tx_current.len,power_slack.len);
-        J(tx_current.begin:tx_current.end, it_slack.begin:it_slack.end) = ...
-            diag(2*st);
-
-        J(real_voltage.begin:real_voltage.end, dv.begin:dv.end) = ...
-            [eye(dv.len);zeros(real_voltage.len,dv.len)]);
-        J(real_voltage.begin:real_voltage.end, dit_real.begin:dit_real.end) = ...
-            ;
-        J(real_voltage.begin:real_voltage.end, dit_imag.begin:dit_imag.end) = ...
-            ;
-        J(real_voltage.begin:real_voltage.end, dir_real.begin:dir_real.end) = ...
-            ;
-        J(real_voltage.begin:real_voltage.end, dir_imag.begin:dir_imag.end) = ...
-            ;
-        J(real_voltage.begin:real_voltage.end, power_slack.begin:power_slack.end) = ...
-            zeros(real_voltage.len,power_slack.len);
-        J(real_voltage.begin:real_voltage.end, it_slack.begin:it_slack.end) = ...
-            zeros(real_voltage.len,it_slack.len);
-
-
-        %next solution
-        x = x - pinv(J)*F;
-    end
 end
--- trash/fineAdjustTest.m
+++ trash/fineAdjustTest.m
@@ -4,16 +4,28 @@ nt = randi(10);
 nr = randi(10);
 timeLine = randomTimeLine(nt,nr,1,zeros(nr,1),rand,rand);
 Z = timeLine.Z;
-ZT = Z(1:nt,1:nt);
-M = Z(nt+1:end,1:nt);
-ZR = Z(nt+1:end,nt+1:end);
 V = 15*rand(nt,1);
 I = Z\[V;zeros(nr,1)];
 IT = I(1:nt);
 IR = I(nt+1:end);
 
-%The expansion of the effect of noise for current amplitude
-dIR = 0.01*rand(nr,1).*IR; % noise of 1% magnitude
-re_dIR = (abs(IR+dIR).^2 - abs(IR).^2 - 2*imag(dIR).*imag(IR))./(2*real(IR))
-real(dIR)
+iZ = eye(nt+nr)/Z;
+ZT = iZ(1:nt, 1:nt);
+ZR = iZ(nt+1:end, 1:nt);
 
+dV = 0.01*max(V)*(rand(nt,1)-0.5);
+x = [dV; 0; rand(nt,1)];
+
+dIT = ZT*dV;
+dIR = ZR*dV;
+
+for t=1:nt
+	z = [ZT(t,:), 0, zeros(1,nt)];
+	(IT(t)'*z + (IT(t).')*((z').'))*x + x.'*z'*z*x + abs(IT(t))^2 - abs(IT(t)+dIT(t))^2
+end
+
+disp('P:');
+Z3 = [ZT, zeros(nt,nt+1);
+	zeros(nt+1,nt), zeros(nt+1)];
+
+V.'*IT + [V.'*ZT+IT.',0,zeros(1,nt)]*x + x.'*Z3*x - (V+dV).'*(IT+dIT)
